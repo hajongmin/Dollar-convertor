0cacfad60c3856adbb7882a0b47b6d688783e36e
hajongmin <62324276+hajongmin@users.noreply.github.com>
2020-03-19 17:34:11 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I'm a new java programmer");
+
+	}
+
+}
