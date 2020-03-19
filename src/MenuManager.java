a17d7a8b63f784a92c7f8d1cca7c78473e5174e3
user <user@USER-PC>
2020-03-19 18:48:28 +0900

    create a program menu

---
+++ src/MenuManager.java
@@ -0,0 +1,16 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(" -- Dollar Conversion Program  -- ");
+		System.out.println("   What do you want to do? " );
+		System.out.println(" 1. Conversion to Dollar ");
+		System.out.println(" 2. Conversion to Won ");
+		System.out.println(" 3. Edit Dollar Exchang Rate ");
+		System.out.println(" 4. View the Rate of change  ");
+		System.out.println(" 5. Exit                     " );               
+
+	}
+
+}
