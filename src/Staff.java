543e5b7cf44053df686f1c790d054bbd91c5cd95
82109 <82109@DESKTOP-TTC47EK>
2020-04-07 18:19:31 +0900

    Add the ArrayList

--- src/Staff.java
+++ src/Staff.java
@@ -1,26 +1,22 @@
-
 public class Staff {
-	String name;
+	String name;	
 	int age;
 	int phonenumber;
 	int workhour;
-	
-	public Staff()
-	{
-		
+
+	public Staff() {
+
 	}
-	
-	
-	public Staff(String name, int age, int phonenumber, int workhour)
-	{
+
+	public Staff(String name, int age, int phonenumber, int workhour) {
 		this.name = name;
 		this.age = age;
 		this.phonenumber = phonenumber;
 		this.workhour = workhour;
 	}
-	
-	public void printinfo()
-	{
-		System.out.print("name : " + name +"\n"+ "age : " + age +"\n"+ "phonenumer : " + phonenumber+"\n" + "workhour : " + workhour+"\n");
+
+	public void printinfo() {
+		System.out.print("name : " + name + ", " + "age : " + age + ", " + "phonenumer : " + phonenumber + ", "
+				+ "workhour : " + workhour + "\n\n");
 	}
-}
+} 
--- src/menuManager.java
+++ src/menuManager.java
@@ -1,6 +1,3 @@
-
-
-
 import java.util.Scanner;
 
 public class menuManager {
@@ -8,48 +5,43 @@ public class menuManager {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		StaffManager staffmanager = new StaffManager(input);
-		
-		
-		int num  = 4;
-		while(num !=5 ) {
-			
+
+		int num = 4;
+		while (num != 5) {
+
 			System.out.println(" -- part-timeworker Manegement System  -- ");
-			System.out.println("   What do you want to do? " );
+			System.out.println("   What do you want to do? ");
 			System.out.println(" 1. Add Staff ");
 			System.out.println(" 2. Delete Staff ");
-			System.out.println(" 3. Edit Staff  ");
-			System.out.println(" 4. view Staff    " );
-			System.out.println(" 5. Exit the program " );  
-			System.out.println(" Selcet one number between 1 ~ 5 :" ); 
-			
+			System.out.println(" 3. Edit Staff ");
+			System.out.println(" 4. view Staffs ");
+			System.out.println(" 5. Exit the program ");
+			System.out.println(" Selcet one number between 1 ~ 5 :");
+
 			num = input.nextInt();
-			
-			
-			if(num == 1) {
+
+			if (num == 1) {
 				staffmanager.addstaff();
 			}
 			
-			else if(num == 2) {
+			else if (num == 2) {
 				staffmanager.deletestaff();
 			}
-			
-			else if(num == 3) {
+
+			else if (num == 3) {
 				staffmanager.editstaff();
 			}
-			
-			else if(num == 4) {
-				staffmanager.viewstaff();
+
+			else if (num == 4) {
+				staffmanager.viewstaffs();
 			}
-			
-			else  {
+
+			else {
 				continue;
 			}
-			
+
 		}
 
 	}
-	
-	
-		
-		
+
 }
